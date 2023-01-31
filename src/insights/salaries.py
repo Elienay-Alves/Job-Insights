231cf21ffae164bf2c97c0ffe1d5caf72b07fe8d
Elienay Alves <elienay8486@gmail.com>
2023-01-31 13:51:21 -0300

    Requitos 4, 5, 8 e 9

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,84 +1,40 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+   data = read(path)
+   return max([int(job['max_salary']) for job in data if job['max_salary'.isdigit()]])
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+data = read(path)
+return min([int(job['min_salary']) for job in data if job['min_salary'].isdigit()])
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
-
+    existence_validation = "min_salary" in job and "max_salary" in job
+    if not existence_validation:
+        raise ValueError
+    min_salary = job["min_salary"]
+    max_salary = job["max_salary"]
+    if not ((type(min_salary) is str or type(min_salary) is int) and (type(max_salary) is str or type(max_salary) is int)):
+        raise ValueError
+    min_salary = int(job["min_salary"])
+    max_salary = int(job["max_salary"])
+    min_max_validation = min_salary > max_salary
+    if min_max_validation or not ( type(salary) is str or type(salary) is int):
+        raise ValueError
+    return int(min_salary) <= int(salary) <= int(max_salary)
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+   list = []
+   try:
+        list = [job for job in jobs if int(job['min_salary']) <= int(salary) <= int(job['max_salary'])]
+        except TypeError:
+            raise ValueError('Error')
+        finally:
+            return filtered_list
