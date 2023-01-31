e6818021d7d368c68047f84a982aedd60bb8cb9b
Elienay Alves <elienay8486@gmail.com>
2023-01-31 13:22:34 -0300

    Fix Requisito 3

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,13 +1,15 @@
 from typing import List, Dict
 from src.insights.jobs import read
 
+
 def get_unique_industries(path: str) -> List[str]:
     data = read(path)
     ind = set()
     for job in data:
         if job["industry"]:
-            industries.add(job["industry"])
-    return list(industries)
+            ind.add(job["industry"])
+    return list(ind)
+
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     return [job for job in jobs if job["industry"] == industry]
