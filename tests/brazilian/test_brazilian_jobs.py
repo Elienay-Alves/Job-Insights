4c1f3ed11439e77557d7621aff75af6894273880
Elienay Alves <elienay8486@gmail.com>
2023-01-31 15:13:10 -0300

    Fix Requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,9 +2,7 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    'return a dictionary list'
-
-    
+'return a dictionary list'
 result = read_brazilian_file('test/mocks/brazilians_jobs.csv')
 for job in result:
     assert 'salary' in job
