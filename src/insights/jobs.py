1f2422f3659bb7dd662fb65fb1968dcf1477dab1
Elienay Alves <elienay8486@gmail.com>
2023-01-31 12:57:37 -0300

    Fix: Requisito 4

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,25 +11,11 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-   data_jobs = read(path)
-   type_jobs = [job["job_type"] for job in data_jobs]
-   return list(set(type_jobs))
-   raise NotImplementedError
+    data_jobs = read(path)
+    type_jobs = [job["job_type"] for job in data_jobs]
+    return list(set(type_jobs))
+    raise NotImplementedError
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
     raise NotImplementedError
