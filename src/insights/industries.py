915a2b5c29d4fd3c6c13e4b3fb4209a31eacd6bf
Elienay Alves <elienay8486@gmail.com>
2023-01-31 13:19:26 -0300

    Fix Requisito 3 e Requisito 7

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,6 +7,7 @@ def get_unique_industries(path: str) -> List[str]:
     for job in data:
         if job["industry"]:
             industries.add(job["industry"])
+    return list(industries)
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-   
+    return [job for job in jobs if job["industry"] == industry]
